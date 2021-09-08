5d8baf3373860fdf75314bd6286c95c31b4fd3c1
Anthony Lopez <anthony.santillan@itriad.org.br>
2021-09-08 00:38:04 -0400

    refactor: Improving data output

--- ex_4.py
+++ ex_4.py
@@ -1,10 +1,15 @@
-def get_multiple(value: list):
+def get_multiple(value):
     result = 0
     for item in range(1, 10):
-        for multiple in value:
-            if item % multiple == 0:
-                result += item
-    return result
+        if item % 3 == 0 or item % 5 == 0:
+            result += item
+    return result == value
 
 
-print(get_multiple([5, 3]))
+if __name__ == '__main__':
+    print(get_multiple(23))
+
+
+# Obs: Não ficou claro qual era a finalidade da questão.
+# "A implementação deve ser capaz de receber por parametro um numero X se ja
+# retornado a soma de todos os numeros multiplos de 3 ou 5" ???
